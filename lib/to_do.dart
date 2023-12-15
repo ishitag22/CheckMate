6b2ca0ac7b88cd871ad38aac2835b169f32b0985
ishitag22 <ishitagaur4@gmail.com>
2023-12-16 01:58:49 +0530

    Added 'New Task' form
    Signed-off-by: ishitag22 <ishitagaur4@gmail.com>

--- lib/to_do.dart
+++ lib/to_do.dart
@@ -2,15 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class To_do extends StatefulWidget {
-  const To_do({super.key});
+class Todo extends StatefulWidget {
+  const Todo({super.key});
 
   @override
-  State<To_do> createState() => _To_doState();
+  State<Todo> createState() => _TodoState();
 
 }
 
-class _To_doState extends State<To_do> {
+class _TodoState extends State<Todo> {
   String currentDate= DateFormat('dd-MM-yyyy').format(DateTime.now());
   @override
   Widget build(BuildContext context) {
@@ -29,40 +29,78 @@ class _To_doState extends State<To_do> {
                backgroundColor: Colors.deepPurpleAccent,
                 elevation: 0,
                 child: Icon(Icons.add_box_rounded,),
-                onPressed: (){add_task();},
+                onPressed: ()=> add_task(context),
             ),
         ),
       ),
     );
   }
-  add_task(){
+   add_task(BuildContext context){
     var width= MediaQuery.of(context).size.width;
     var height= MediaQuery.of(context).size.height;
-    return AlertDialog(
-      scrollable: true,
-      title: Text('New Task'),
-      content: SizedBox(
-        height: height*0.35,
-        width: width,
-        child: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                hintText: 'Task',
-                icon: Icon(Icons.list_alt_outlined, color: Colors.purple,),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-              )
-        ),
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        scrollable: true,
+        title: Text('New Task'),
+        content: SizedBox(
+          height: height * 0.35,
+          width: width,
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Task',
+                      icon: Icon(
+                        Icons.list_alt_outlined, color: Colors.deepPurpleAccent,),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    )
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      hintText: 'Description',
+                      icon: Icon(CupertinoIcons.chat_bubble_2,
+                        color: Colors.deepPurpleAccent,)
+                  ),
+                ),
+                // SizedBox(height: 1.0,
+                // child:
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: ElevatedButton(onPressed: (){},
+                          child: Text('Submit'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                        ),
 
-        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(onPressed: (){},
+                          child: Text('Cancel'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                      ),
+                    ),
+                    ),
+                  ],
+                ),
+                // ),
+              ],
+            ),
+
           ),
-
         ),
-      ),
+      );
+    }
     );
-  }
-
+   }
 }
 
 
